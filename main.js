df43c3c9617c42ca04eefed9b405ebf198a50939
churmon <churmonmalanda@gmail.com>
2021-02-27 08:08:46 +0200

    Add files via upload

---
+++ main.js
@@ -0,0 +1,16 @@
+$(function(){
+
+$(".img").hover(function(){
+    
+    // $(this).myfunction();
+
+    $(this).$(function($){
+        $(".layer").toggle();
+        // $.fn.myfunction = function() {
+        //     $(".layer").toggle();
+        //     return this;
+        // }; 
+     })( jQuery );
+});
+
+});
